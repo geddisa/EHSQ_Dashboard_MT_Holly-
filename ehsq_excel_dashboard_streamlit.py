5d1483447a63dcff7003921081633de3988152e7
geddisa <aliyah.geddis@centuryaluminum.com>
2026-07-01 11:24:51 -0400

    Fix page title formatting in dashboard setup

--- ehsq_excel_dashboard_streamlit.py
+++ ehsq_excel_dashboard_streamlit.py
@@ -5,7 +5,7 @@ import plotly.graph_objects as go
 import matplotlib.pyplot as plt
 
 # Dashboard Setup
-st.set_page_config(layout="wide", page_title="Century Aluminum | Mt. Holly| EHSQ KPI Dashboard")
+st.set_page_config(layout="wide", page_title="Century Aluminum | Mt. Holly | EHSQ KPI Dashboard")
 st.title("EHSQ KPI Dashboard")
 
 @st.cache_data
